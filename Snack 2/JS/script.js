37349470888a911995f3a2858a9030cc511ef7ad
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-27 15:12:10 +0100

    snack 2 impostato

---
+++ Snack 2/JS/script.js
@@ -0,0 +1,37 @@
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+//creiamo un array di oggetti che comprenda delle squadre di calcio, i punti fatti e i falli subiti
+const squadre = [
+    {
+        nome: 'juve',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'milan',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'inter',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'napoli',
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: 'venezia'
+        punti: 0,
+        falli: 0
+    },
+]
+
+
+//punti e falli verranno riempiti con valori randomici, e perciò di default sono settati a 0 
+//diamo loro dei valori
